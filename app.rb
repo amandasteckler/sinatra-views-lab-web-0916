99eefd1b26cd0856fdfe027e08ff84656d3af7b3
Amanda Steckler <amandajsteckler@gmail.com>
2016-10-13 21:50:00 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,26 @@
+require 'date'
+
 class App < Sinatra::Base
 
 	get '/' do
 		erb :index
 	end
 
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/goodbye' do
+		@name = 'Joe'
+		erb :goodbye
+	end
+
+	get '/date' do
+		@day = Date::DAYNAMES[Date.today.wday]
+		@month = Date::MONTHNAMES[Date.today.month]
+		@day_of_month = Time.now.day
+		@year = Time.now.year
+		erb :date
+	end
 
 end
